c01195cde0583b85b87ffffb6d34f55ef2cf7df8
Mparsons91 <mrparsons91@gmail.com>
2023-04-20 13:32:51 -0400

    Added the post input box, connected to API, saved some assets for the box in ./client/src/assets/

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,37 +1,25 @@
 import Post from "../post/Post";
-import "./posts.scss"
+import "./posts.scss";
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
 
+const Posts = ({userId}) => {
+  const { isLoading, error, data } = useQuery(["posts"], () =>
+    makeRequest.get("/posts?userId="+userId).then((res) => {
+      return res.data;
+    })
+  );
 
+  return (
+    <div className="posts">
+      {error
+        ? "Something went wrong!"
+        : isLoading
+        ? "loading"
+        : data.map((post) => <Post post={post} key={post.id} />)}
+    </div>
+  );
+};
 
-const Posts = () => {
-    //Temporary dummy data
-    const posts = [
-      {
-        id: 1,
-        name: "Flint Marko",
-        userId: 1,
-        profilePic:
-          "https://images.pexels.com/photos/6141908/pexels-photo-6141908.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        desc: "Just chilling like villain on the beach, Having this much fun should be a crime ",
-        img: "https://images.pexels.com/photos/1151282/pexels-photo-1151282.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-      {
-        id: 2,
-        name: "Eobard Thawne",
-        userId: 1,
-        profilePic:
-          "https://images.pexels.com/photos/1114690/pexels-photo-1114690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        desc: "It was me BARRY !",
-      },
-    ];
-  
-    return (
-      <div className="posts">
-        {posts.map((post) => (
-          <Post post={post} key={post.id} />
-        ))}
-      </div>
-    );
-  };
-  export default Posts;
+export default Posts;
   
