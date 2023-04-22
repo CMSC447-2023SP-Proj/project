f92879eb977f6df44beb66297e09a980a7a1526e
willia13 <113810608+willia13@users.noreply.github.com>
2023-04-22 16:38:35 -0400

    added link to settings page

--- client/src/pages/settings/settings.jsx
+++ client/src/pages/settings/settings.jsx
@@ -1,86 +1,69 @@
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./register.scss";
-import { useState } from "react";
-import axios from "axios";
+import { AuthContext } from "../../context/authContext";
+import "./settings.scss";
 
-const Register = () => {
+const Settings = () => {
     const [inputs, setInputs] = useState({
-      username: "",
-      password: "",
-      email: "",
-      name: "",
+        username: "",
+        password: "",
     });
     const [err, setErr] = useState(null);
-  
+
     const navigate = useNavigate()
 
     const handleChange = (e) => {
-      setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-      console.log(inputs);
+        setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
     };
-  
-    const handleClick = async (e) => {
-      e.preventDefault();
-  
-      try {
-        await axios.post("http://localhost:8800/api/auth/register", inputs);
-        navigate("/");
-      } catch (err) {
-        setErr(err.response.data);
-      }
-    };
-  
-    console.log(err)
+    const { login } = useContext(AuthContext);
 
-    return(
-        <div className="register">
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await login(inputs);
+            navigate("/")
+        } catch (err) {
+            setErr(err.response.data);
+        }
+    };
+
+    return (
+        <div className="login">
             <div className="card">
                 <div className="left">
-                  <h1>Join the Family !</h1>
-                  <p> Do you wanna join the newest social media platform,
-                    Its sweeping the nation,
-                    CODED BY UMBC STUDENTS, FOR UMBC STUDENTS,
-                    Join today 
-                  </p>
-                    <span> Do you have an account?</span>
-                   <Link to="/login">
-                    <button>Login</button>
+                    <h1>Settings</h1>
+                    <p>
+                        bbbooboooedfjsfj,
+                        alias totam numquam ipsa exercitationem dignissimos, error nam,
+                        consequatur.
+                    </p>
+                    <span>Don't you have an account?</span>
+                    <Link to="/register">
+                        <button>Register</button>
                     </Link>
-                    
                 </div>
                 <div className="right">
-                <h1>Register</h1>
-                <form>
-                    <input 
-                      type="text" 
-                      placeholder="Username"
-                      name="username"
-                      onChange={handleChange} 
-                    />
-                    <input 
-                      type="password" 
-                      placeholder="Password"
-                      name="password"
-                      onChange={handleChange} 
-                    />
-                    <input 
-                      type="email" 
-                      placeholder="Email" 
-                      name="email"
-                      onChange={handleChange}
-                    />
-                    <input 
-                      type="text" 
-                      placeholder="Name"
-                      name="name"
-                      onChange={handleChange} 
-                    />
-                    {err && err}
-                    <button onClick={handleClick}>Register</button>
-                </form>
-              </div>
-          </div>
+                    <h1>Login</h1>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            name="username"
+                            onChange={handleChange}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            onChange={handleChange}
+                        />
+                        {err && err}
+                        <button onClick={handleLogin}>Login</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
-}
-export default Register;
+};
+
+export default Settings;
